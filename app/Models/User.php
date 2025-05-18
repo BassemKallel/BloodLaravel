98ae79e4521f2ed5ea8160caced74fbe90ce426e
Bassem Kallel <kallelbassem@bizerte.r-iset.tn>
2025-05-18 21:58:43 +0100

    corriger model user failable

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'groupeSanguin',
+        'dateNaissance',
+        'sexe',
+        'address',
+        'phone',
+        'role',
     ];
 
     /**
